e71eb5329bcd882c693cdd3f4563308e63247445
LeGiangK62 <tung.giangle99@gmail.com>
2025-07-23 21:11:50 +0900

    SaveCheckpoint

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from torch import nn, optim
 import numpy as np
 
 
-from utils import train_graph, test_graph, EarlyStopping
+from utils import train_graph, test_graph, EarlyStopping, save_checkpoint
 from data import load_dataset, eval_dataset, random_split
 from model import QGNNGraphClassifier, QGNNNodeClassifier
 from test import HandcraftGNN, HandcraftGNN_NodeClassification
@@ -24,6 +24,7 @@ os.makedirs(result_dir, exist_ok=True)
 os.makedirs(os.path.join(result_dir, 'fig'), exist_ok=True)
 os.makedirs(os.path.join(result_dir, 'log'), exist_ok=True)
 os.makedirs(os.path.join(result_dir, 'model'), exist_ok=True)
+os.makedirs(os.path.join(result_dir, 'train_plot'), exist_ok=True)
 
 param_file = os.path.join(result_dir, 'log', f"{timestamp}_model_parameters.txt")
 grad_file = os.path.join(result_dir, 'log', f"{timestamp}_model_gradients.txt")
@@ -49,6 +50,8 @@ def get_args():
 
     
     # Debug options
+    parser.add_argument('--pre_train', type=str, default=None, help='Load the pre-trained model (timestamp as name)')
+    parser.add_argument('--continue_train', action='store_true', help='Continue training from pre-trained model')
     parser.add_argument('--plot', action='store_true', help='Enable plotting')
     parser.add_argument('--save_model', action='store_true', help='Enable saving model')
     parser.add_argument('--gradient', action='store_true', help='Enable gradient saving')
@@ -95,6 +98,11 @@ def main(args):
         test_size=args.test_size,
         batch_size=args.batch_size
     )
+    
+    result_base = f"{timestamp}_{args.model}_{args.graphlet_size}_{args.epochs}_{args.lr}"
+    plot_train_path = os.path.join(result_dir, 'fig', f"plot_{result_base}_train.png")
+    npz_path = os.path.join(result_dir, 'train_plot', f"data_{result_base}.npz")
+    model_save = os.path.join(result_dir, 'model', f"model_{result_base}.pt")
 
     # if task_type != 'graph':
     #     raise NotImplementedError("Node classification support is not implemented yet.")
@@ -267,76 +275,118 @@ def main(args):
     start = time.time()
     step_plot = args.epochs // 10 if args.epochs > 10 else 1
     
-    model_save = os.path.join(result_dir, 'model', f"{timestamp}_{args.model}_{args.dataset.lower()}.pt")
-    early_stopping = EarlyStopping(patience=10, save_path=model_save)
+    # early_stopping = EarlyStopping(patience=10, save_path=model_save)
     
-    print(f"{timestamp} \n")
+    print(f"\n ===={timestamp}==== ")
+    
+    if args.pre_train is not None:
+        pre_trained_path = os.path.join(result_dir, 'model', f"model_{args.pre_train}.pt")
+        checkpoint = torch.load(pre_trained_path, map_location='cpu')
+        model.load_state_dict(checkpoint['model_state_dict'])
+        
+        pre_train_npz_path = os.path.join(result_dir, 'train_plot', f"data_{args.pre_train}.npz")
+        data = np.load(pre_train_npz_path)
+        pre_train_epoch = data['epoch'].shape[0]          
+        train_losses = data['train_losses'].tolist()
+        test_losses = data['test_losses'].tolist()
+        train_accs = data['train_accs'].tolist()
+        test_accs = data['test_accs'].tolist()
+        print(f"Pre-trained model loaded from {pre_trained_path} with {pre_train_epoch} epochs.")
+        if not args.continue_train: 
+            model.eval()
+            print("Skip training...")
+        else:
+            print(f"Continuing training model with {args.graphlet_size} graphlet size with {args.epochs} epochs, "
+            f"learning rate {args.lr}, step size {args.step_size}, and gamma {args.gamma}.")
+    else: 
+        print(f"Training model with {args.graphlet_size} graphlet size with {args.epochs} epochs, "
+            f"learning rate {args.lr}, step size {args.step_size}, and gamma {args.gamma}.")
+            
+        
+        
+        
     print(f"Training model {args.model} on {args.dataset} with {args.graphlet_size} graphlet size with {args.epochs} epochs, "
           f"learning rate {args.lr}, step size {args.step_size}, and gamma {args.gamma}.")
+    if args.continue_train or args.pre_train is None:
     
-    if args.task == 'graph':
-        for epoch in range(1, args.epochs + 1):
-            train_graph(model, optimizer, train_loader, criterion, device)
-            train_loss, train_acc, f1_train = test_graph(model, train_loader, criterion, device, num_classes)
-            test_loss, test_acc, f1_test = test_graph(model, test_loader, criterion, device, num_classes)
-            scheduler.step()
-            # if args.save_model:
-            #     early_stopping(test_loss, model)
-            train_losses.append(train_loss)
-            test_losses.append(test_loss)
-            train_accs.append(train_acc)
-            test_accs.append(test_acc)
-            ############
-            if args.gradient:
-                # === Write model parameters to file ===
-                with open(param_file, "a") as f_param:
-                    f_param.write("="*40 + f" Epoch {epoch} " + "="*40 + "\n")
-                    for name, param in model.named_parameters():
-                        f_param.write(f"{name}:\n{param.data.cpu().numpy()}\n\n")
+        if args.task == 'graph':
+            for epoch in range(1, args.epochs + 1):
+                train_graph(model, optimizer, train_loader, criterion, device)
+                train_loss, train_acc, f1_train = test_graph(model, train_loader, criterion, device, num_classes)
+                test_loss, test_acc, f1_test = test_graph(model, test_loader, criterion, device, num_classes)
+                scheduler.step()
+                if args.save_model:
+                    # early_stopping(-avg_test_sinr, model)
+                    save_checkpoint(model, optimizer, model_save)
+                train_losses.append(train_loss)
+                test_losses.append(test_loss)
+                train_accs.append(train_acc)
+                test_accs.append(test_acc)
+                np.savez_compressed(
+                    npz_path, 
+                    epoch=np.arange(1, epoch+2),
+                    train_losses=np.array(train_losses),
+                    test_losses=np.array(test_losses),
+                    train_accs=np.array(train_accs),
+                    test_accs=np.array(test_accs),
+                )
+                ############
+                if args.gradient:
+                    # === Write model parameters to file ===
+                    with open(param_file, "a") as f_param:
+                        f_param.write("="*40 + f" Epoch {epoch} " + "="*40 + "\n")
+                        for name, param in model.named_parameters():
+                            f_param.write(f"{name}:\n{param.data.cpu().numpy()}\n\n")
 
-                # === Write gradients to separate file ===
-                with open(grad_file, "a") as f_grad:
-                    f_grad.write("="*40 + f" Epoch {epoch} " + "="*40 + "\n")
-                    for name, param in model.named_parameters():
-                        if param.requires_grad:
-                            if param.grad is None:
-                                f_grad.write(f"{name}: No gradient (None)\n")
-                            else:
-                                grad = param.grad.cpu().numpy()
-                                f_grad.write(f"{name}:\n{grad}\n\n")
-            ############
-            if epoch % step_plot == 0:
-                print(f"Epoch {epoch:02d} | Train Loss: {train_loss:.4f}, Acc: {train_acc:.4f} | "
-                    f"Test Loss: {test_loss:.4f}, Acc: {test_acc:.4f}")
-    else:  # node task
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer, 
-            mode='min',                           
-            factor=args.gamma,      # Multiplies LR by this factor (e.g., 0.5)
-            patience=args.epochs//10,# Wait this many epochs without improvement
-            # verbose=True                            # Print updates
-        )
-        from utils import train_node, test_node
-        for epoch in range(1, args.epochs + 1):
-            train_loss = train_node(model, optimizer, data, criterion, device)
-            test_metrics = test_node(model, data, criterion, device, num_classes)
-            train_losses.append(test_metrics['train']['loss'])
-            test_losses.append(test_metrics['test']['loss'])
-            train_accs.append(test_metrics['train']['acc'])
-            test_accs.append(test_metrics['test']['acc'])
-            
-            if args.save_model:
-                early_stopping(test_losses[-1], model)
+                    # === Write gradients to separate file ===
+                    with open(grad_file, "a") as f_grad:
+                        f_grad.write("="*40 + f" Epoch {epoch} " + "="*40 + "\n")
+                        for name, param in model.named_parameters():
+                            if param.requires_grad:
+                                if param.grad is None:
+                                    f_grad.write(f"{name}: No gradient (None)\n")
+                                else:
+                                    grad = param.grad.cpu().numpy()
+                                    f_grad.write(f"{name}:\n{grad}\n\n")
+                ############
+                if epoch % step_plot == 0:
+                    print(f"Epoch {epoch:02d} | Train Loss: {train_loss:.4f}, Acc: {train_acc:.4f} | "
+                        f"Test Loss: {test_loss:.4f}, Acc: {test_acc:.4f}")
+        else:  # node task
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer, 
+                mode='min',                           
+                factor=args.gamma,      # Multiplies LR by this factor (e.g., 0.5)
+                patience=args.epochs//10,# Wait this many epochs without improvement
+                # verbose=True                            # Print updates
+            )
+            from utils import train_node, test_node
+            for epoch in range(1, args.epochs + 1):
+                train_loss = train_node(model, optimizer, data, criterion, device)
+                test_metrics = test_node(model, data, criterion, device, num_classes)
+                train_losses.append(test_metrics['train']['loss'])
+                test_losses.append(test_metrics['test']['loss'])
+                train_accs.append(test_metrics['train']['acc'])
+                test_accs.append(test_metrics['test']['acc'])
                 
-            scheduler.step(test_metrics['val']['loss'])
-            if epoch % step_plot == 0:
-                print(f"Epoch {epoch:02d} | Train Loss: {train_loss:.4f} |" +
-                    f"Train Acc: {test_metrics['train']['acc']:.4f} | "
-                    f"Val Acc: {test_metrics['val']['acc']:.4f} | Test Acc: {test_metrics['test']['acc']:.4f}")
+                if args.save_model:
+                    # early_stopping(test_losses[-1], model)
+                    save_checkpoint(model, optimizer, model_save)
+                                        
+                scheduler.step(test_metrics['val']['loss'])
+                if epoch % step_plot == 0:
+                    print(f"Epoch {epoch:02d} | Train Loss: {train_loss:.4f} |" +
+                        f"Train Acc: {test_metrics['train']['acc']:.4f} | "
+                        f"Val Acc: {test_metrics['val']['acc']:.4f} | Test Acc: {test_metrics['test']['acc']:.4f}")
+    if args.save_model:
+            print(f"Model checkpoint saved to {model_save}")
     end = time.time()
     print(f"Total execution time: {end - start:.6f} seconds")
     if args.plot:
-        epochs_range = range(1, args.epochs + 1)
+        if args.pre_train is None:
+            pre_train_epoch = 0
+        total_epoch = args.epochs + pre_train_epoch
+        epochs_range = range(1, total_epoch + 1)
 
         plt.figure(figsize=(10, 5))
         plt.suptitle(f"{args.model.upper()} on {args.dataset.upper()}", fontsize=14)
@@ -359,8 +409,8 @@ def main(args):
         # plt.tight_layout()
         plt.tight_layout(rect=[0, 0, 1, 0.95]) 
         # plot_path = f"plot_{args.model}_{args.graphlet_size}_{args.dataset.lower()}_{args.epochs}epochs_lr{args.lr}_{args.gamma}over{args.step_size}.png"
-        plot_path = f"plot_{timestamp}_{args.model}_{args.graphlet_size}_{args.dataset.lower()}_{args.epochs}epochs_lr{args.lr}_{args.gamma}over{args.step_size}.png"
-        plt.savefig(os.path.join('../results/fig', plot_path), dpi=300)
+        # plot_path = f"plot_{timestamp}_{args.model}_{args.graphlet_size}_{args.dataset.lower()}_{args.epochs}epochs_lr{args.lr}_{args.gamma}over{args.step_size}.png"
+        plt.savefig(plot_train_path, dpi=300)
         
     if args.results:
         accuracies = []
